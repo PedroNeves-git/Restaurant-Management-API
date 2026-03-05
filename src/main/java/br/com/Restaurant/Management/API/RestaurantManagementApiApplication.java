3d31f34f95f2122a0c9d5cf319bceb547531a331
Pedro Neves <sgtpedrocos@gmail.com>
2026-03-04 22:34:10 -0300

    feat: inicial commit

---
+++ src/main/java/br/com/Restaurant/Management/API/RestaurantManagementApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.Restaurant.Management.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantManagementApiApplication.class, args);
+	}
+
+}
